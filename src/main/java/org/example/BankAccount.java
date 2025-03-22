00dd948628d215455bdb512fc5cc0ba41bd00cd4
hellodileep <hellodileep@gmail.com>
2025-03-23 00:15:26 +0530

    initial commit

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,8 +1,32 @@
 package org.example;
 
-@Data
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class BankAccount {
-    String accountNo;
-    String accountName;
-    double balance;
+    private double balance;
+
+    public BankAccount(double initialBalance){
+        this.balance = initialBalance;
+    }
+
+    public synchronized void deposit(double depositAmount){
+        if(depositAmount>0) {
+            this.balance += depositAmount;
+            log.info("successfully deposited amount :{}",depositAmount);
+        }
+        else System.out.println("Invalid deposit amount");
+    }
+
+    public synchronized void withdraw(double withdrawAmount){
+        if(withdrawAmount >0) {
+            if (withdrawAmount < this.balance) {
+                balance = balance - withdrawAmount;
+                log.info("you withdrawn $ {} and the new balance is $ {}", withdrawAmount, balance);
+            } else log.info("insufficient balance");
+        }
+        else
+            log.info("Invalid amount");
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
+import javafx.concurrent.Task;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //ThreadPoolExecutor  threadPoolExecutor = new ThreadPoolExecutor();
+        ExecutorService exs = Executors.newCachedThreadPool();
+        BankAccount bankAccount = new BankAccount(0);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter deposit amount :");
+            if (scanner.hasNextDouble()) {
+                double depositAmount = scanner.nextDouble();
+                Runnable task = () -> {
+                    bankAccount.deposit(depositAmount);
+                };
+                exs.execute(task);
+            } else throw new Exception("invalid deposit amount");
+            //System.out.println(depositAmount);
+            System.out.println("Enter withdrawal amount :");
+            if (scanner.hasNextDouble()) {
+                double withDrawAmount = scanner.nextDouble();
+                Runnable task2 = () -> {
+                    bankAccount.withdraw(withDrawAmount);
+                };
+                exs.execute(task2);
+            } else  throw new Exception("invalid withdrawal amount");
+        }catch(Exception e){
+            log.error("Exception",e);
+            //throw new RuntimeException(e);
         }
     }
 }
